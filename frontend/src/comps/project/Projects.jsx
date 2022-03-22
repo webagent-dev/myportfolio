9a011b01a3dd098b7b1bec72d7ec77a5928167e3
WebAgent-dev <webagent24@gmail.com>
2022-03-22 06:17:40 +0100

    working on my project component

--- frontend/src/comps/project/Projects.jsx
+++ frontend/src/comps/project/Projects.jsx
@@ -1,12 +1,33 @@
 import React, { useContext } from 'react'
-import { ProjectContainer, HeaderContainer, Header, Info, Wrapper } from './project'
+import { ProjectContainer, HeaderContainer, Header, Info, Wrapper, SelectContainer, SortHeader,} from './project'
 import { BtnList, ProjectList } from '../../one'
 import { ThemeContext } from '../../context/ThemeContext'
-
+import { port } from '../../data'
+import Select from 'react-select'
+const options = [
+  { value: 'html&css', label: 'Html&Css' },
+  { value: 'javascript', label: 'Javascript' },
+  { value: 'reactjs', label: 'Reactjs' },
+    { value: 'jamstack', label: 'Jam-Stack' },
+      { value: 'nodejs', label: 'Nodejs' },
+        { value: 'nextjs', label: 'Nextjs' }
+]
+const style ={
+  fontSize: "20px",
+  textTransform: "uppercase",
+  backgroundColor: 'white', 
+  color: 'skyblue'
+}
+const mainStyle = {
+  select: styles => ({...styles,   fontSize: "20px",textTransform: "uppercase",backgroundColor:'white', 
+  color: 'skyblue'}),
+  control: styles => ({...styles, backgroundColor: 'skyblue',}),
+  option: styles => ({...styles,  fontFamily: " 'Source Code Pro', monospace", textTransform: "uppercase",  fontSize: '15px',  padding: "12px  24px"})
+}
 
 function Projects() {
   const {items, cat, setCat } = useContext(ThemeContext)
-  
+  console.log(port)
   return (
     <ProjectContainer>
         <HeaderContainer>
@@ -16,6 +37,15 @@ function Projects() {
             Cause it takes insight and keen knowledge to accomplish what I do </Info>
         </HeaderContainer>
         <Wrapper>
+          <SelectContainer>
+            <SortHeader>Sort Tech</SortHeader>
+               <Select 
+               options={options}
+              //  style={style.Select}
+                styles={mainStyle} 
+                placeholder='Select Stack'
+                />
+          </SelectContainer>
             <BtnList />
             <ProjectList />
         </Wrapper>
--- frontend/src/comps/project/project.js
+++ frontend/src/comps/project/project.js
@@ -31,4 +31,44 @@ export const HeaderContainer = styled.div`
     line-height: 25px;
     text-align: center;
   `
-  export const Wrapper = styled.div``
+  export const Wrapper = styled.div`
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    align-items: center;
+    gap: 1rem;
+  `
+  export const SelectContainer = styled.div`
+  `
+  export const Sort = styled.div`
+    display: flex;
+    width: 400px;
+`
+export const OptionContainer = styled.div`
+  background: skyblue;
+  color: #f5f6fa;
+  /* max-height: 0; */
+  width: 100%;
+  /* opacity: 0; */
+  transition: all 0.4s linear;
+  border-radius: 8px;
+  overflow: hidden; 
+`
+export const Option = styled.div`
+padding: 12px 24px;
+cursor: pointer;
+&:hover{
+  background: lightgray;
+}
+`
+export const Input = styled.input`
+  display: none;
+`
+export const Label = styled.label`
+cursor: pointer;
+`
+export const SortHeader = styled.h1`
+    text-transform: uppercase;
+    font-family: 'Source Code Pro', monospace;
+    color: skyblue;
+`
